7bb899d2b6704e401c0896609b29f6eadd465902
nguyenanninh1212212 <ninh56085@gmail.com>
2025-04-11 16:42:14 +0700

    add

--- src/api/stories.ts
+++ src/api/stories.ts
@@ -1,5 +1,10 @@
 import { api } from ".";
 
+export const getHomepage = async () => {
+  const result = await api.get("/homepage");
+  return result.data;
+};
+
 export const getStories = async (page: number, limit: number) => {
   const result = await api.get("/story", {
     params: { page, limit },
