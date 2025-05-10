3597db309798fc63ad73578dbbfc9724c8cbfaaf
nguyenanninh1212212 <ninh56085@gmail.com>
2025-05-11 00:11:34 +0700

    add

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://localhost:8081";
 export const api = axios.create({
   baseURL: `${BASE_URL}`, // Địa chỉ API backend
   timeout: 5000, // Thời gian timeout
